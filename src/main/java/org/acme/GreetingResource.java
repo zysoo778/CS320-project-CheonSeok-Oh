9e46771c659b3487a12153833df9ab4eb5270959
CheonSeok <oh245@ensign.edu>
2025-01-07 18:01:13 -0700

    Initial commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,9 +1,11 @@
 package org.acme;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.PathParam;
 
 @Path("/hello")
 public class GreetingResource {
@@ -12,5 +14,40 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @Path("/personalized/{name}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHello(@PathParam("name") String name) {
+        return "Hello " + name;
+    }
+
+    @POST
+    @Path("/personalized")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHelloPost(Person p) {
+        return "Hello " + p.getFirst() + " " + p.getLast();
+    }
+
+    public static class Person {
+        private String first;
+        private String last;
+
+        public String getFirst() {
+            return first;
+        }
+
+        public void setFirst(String first) {
+            this.first = first;
+        }
+
+        public String getLast() {
+            return last;
+        }
+
+        public void setLast(String last) {
+            this.last = last;
+        }
     }
 }
